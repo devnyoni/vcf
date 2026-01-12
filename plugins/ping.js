ff536725918ac48b59f1354c5f439cc8c7941a5c
devnyoni <devnyoni@gmail.com>
2026-01-12 21:14:01 +0300

    Delete plugins directory

--- plugins/ping.js
+++
@@ -1,9 +0,0 @@
- module.exports = {
-    name: 'ping',
-    async execute(sock, from) {
-        const start = Date.now();
-        const { key } = await sock.sendMessage(from, { text: 'Testing...' });
-        const end = Date.now();
-        await sock.sendMessage(from, { text: `Pong! 🚀\nSpeed: ${end - start}ms`, edit: key });
-    }
-};
--- plugins/restart.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'restart',
-    async execute(sock, from) {
-        const { key } = await sock.sendMessage(from, { text: 'Restarting NYONI-XMD...' });
-        await sock.sendMessage(from, { text: 'Bot restarted successfully! ✅', edit: key });
-        process.exit(); // Render itaiwasha upya yenyewe
-    }
-};
